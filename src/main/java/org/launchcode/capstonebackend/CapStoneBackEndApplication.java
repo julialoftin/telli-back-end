daeb850f8334cabe7a9d214f1a2af155d6fbacec
Julia Loftin <julia.loftin7@gmail.com>
2024-01-13 17:06:43 -0600

    initializes spring project

---
+++ src/main/java/org/launchcode/capstonebackend/CapStoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.capstonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoneBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/capstonebackend/CapStoneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.capstonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
