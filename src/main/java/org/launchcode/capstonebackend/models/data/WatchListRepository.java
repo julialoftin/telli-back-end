5de7e2e4c524f784781d4721a51ea6443092377d
Julia Loftin <julia.loftin7@gmail.com>
2024-01-19 09:49:56 -0600

    creates findAllAsList within watchListRepository and created WatchList get mapping

--- src/main/java/org/launchcode/capstonebackend/models/data/WatchListRepository.java
+++ src/main/java/org/launchcode/capstonebackend/models/data/WatchListRepository.java
@@ -4,6 +4,11 @@ import org.launchcode.capstonebackend.models.WatchList;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WatchListRepository extends CrudRepository<WatchList, Integer> {
+
+    List<WatchList> findAllAsList();
+
 }
