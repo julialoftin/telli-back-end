7f8dbfb01c503a2e9f421816c73fb3d4fd0bcfca
Jessica <jlknobeloch@gmail.com>
2024-01-19 19:15:59 -0600

    created User model

---
+++ src/main/java/org/launchcode/capstonebackend/models/User.java
@@ -0,0 +1,25 @@
+package org.launchcode.capstonebackend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
