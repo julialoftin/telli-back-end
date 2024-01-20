280b9705867c7abb3cdfe64b4dd76816bff4ed1b
Jessica <jlknobeloch@gmail.com>
2024-01-19 21:59:51 -0600

    created User Repository

---
+++ src/main/java/org/launchcode/capstonebackend/models/data/UserRepository.java
@@ -0,0 +1,9 @@
+package org.launchcode.capstonebackend.models.data;
+
+import org.launchcode.capstonebackend.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
