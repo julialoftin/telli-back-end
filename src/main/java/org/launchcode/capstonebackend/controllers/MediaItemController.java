99058de4a93592c259e177eeff70736efb3718fd
Julia Loftin <julia.loftin7@gmail.com>
2024-01-25 14:17:52 -0600

    creates ability to associate media id with watchlist id in database, creates endpoint

--- src/main/java/org/launchcode/capstonebackend/controllers/MediaItemController.java
+++ src/main/java/org/launchcode/capstonebackend/controllers/MediaItemController.java
@@ -1,11 +1,17 @@
 package org.launchcode.capstonebackend.controllers;
 
+import org.launchcode.capstonebackend.models.MediaItem;
 import org.launchcode.capstonebackend.models.WatchList;
 import org.launchcode.capstonebackend.models.data.MediaItemRepository;
 import org.launchcode.capstonebackend.models.data.WatchListRepository;
+import org.launchcode.capstonebackend.models.dto.MediaItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -18,10 +24,32 @@ public class MediaItemController {
     @Autowired
     WatchListRepository watchListRepository;
 
-    @PostMapping("/add-media-item-to-watchlist/{watchListId}/{tmdbId}/{mediaType}")
-    public ResponseEntity<WatchList> addMediaItemToWatchList(@PathVariable int watchListId,
-                                                             @PathVariable int tmdbId,
-                                                             @PathVariable String mediaType) {
+    private MediaItem convertMediaItemDTOToEntity(MediaItemDTO mediaItemDTO) {
+        MediaItem mediaItem = new MediaItem(mediaItemDTO.getTmdbId(), mediaItemDTO.getMediaType());
+        return mediaItem;
+    }
+
+    @PostMapping("/add-media-item-to-watchlist/{watchListId}")
+    public ResponseEntity<List<MediaItem>> addMediaItemToWatchList(@PathVariable int watchListId,
+                                                                   @RequestBody MediaItemDTO mediaItemDTO,
+                                                                   Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Optional<WatchList> optionalWatchList = watchListRepository.findById(watchListId);
+        if (optionalWatchList.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            MediaItem mediaItem = convertMediaItemDTOToEntity(mediaItemDTO);
+            mediaItemRepository.save(mediaItem);
+
+            WatchList watchList = optionalWatchList.get();
+            watchList.addMediaItemToList(mediaItem);
+            watchListRepository.save(watchList);
+
+            return ResponseEntity.ok(watchList.getMediaItems());
+        }
 
     }
 
