aa8102794b29f6d66e325f4bc06dee2444a13d35
Julia Loftin <julia.loftin7@gmail.com>
2024-01-27 11:39:53 -0600

    adds Repository annotation to MediaItemRepository

--- src/main/java/org/launchcode/capstonebackend/models/data/MediaItemRepository.java
+++ src/main/java/org/launchcode/capstonebackend/models/data/MediaItemRepository.java
@@ -2,6 +2,8 @@ package org.launchcode.capstonebackend.models.data;
 
 import org.launchcode.capstonebackend.models.MediaItem;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MediaItemRepository extends CrudRepository<MediaItem, Integer> {
 }
